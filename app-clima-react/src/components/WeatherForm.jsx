07cbc472ed590ed49ec2179bc67bc1dae52590e9
Dixon G <dxxn0101@gmail.com>
2023-03-26 09:49:17 -0600

    add responsive design and change button and background styles

--- app-clima-react/src/components/WeatherForm.jsx
+++ app-clima-react/src/components/WeatherForm.jsx
@@ -37,7 +37,7 @@ export const WeatherForm = ({ onChangeCity, inputRef }) => {
           onChange={onChange}
           placeholder="Search a city"
         />
-        <button className="mt-2 w-50 btn btn-dark">Search</button>
+        <button className={`${styles.submitBtn} btn`}>Search</button>
       </form>
     </>
   );
